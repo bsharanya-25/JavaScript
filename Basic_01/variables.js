b26166685830a4bd25c74c97fe025e4364cbfb27
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2024-06-01 13:49:47 +0000

    Container config done

---
+++ Basic_01/variables.js
@@ -0,0 +1,16 @@
+const accountId= 121234;
+let accountEmailId="sharu@gamil.com"
+var accountName="Sharanya"
+accountCity ="Bangalore"
+let accountState;
+
+
+console.table([accountEmailId, accountId, accountName, accountCity, accountState]);
+
+
+
+/*
+to declare const we use const keyword
+to declare variables we use let or var keyword
+Prefer not to use var because it cause issue in block and functional scope
+ */
